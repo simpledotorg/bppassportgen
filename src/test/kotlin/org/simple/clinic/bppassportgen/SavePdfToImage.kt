5bd0d00cbc7fdc848f40a840e758b03dd4577aa5
Vinay S Shenoy <vinay@obvious.in>
2019-09-25 14:33:57 +0530

    Add class to render PDF into images

---
+++ src/test/kotlin/org/simple/clinic/bppassportgen/SavePdfToImage.kt
@@ -0,0 +1,14 @@
+package org.simple.clinic.bppassportgen
+
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.rendering.PDFRenderer
+import java.awt.image.BufferedImage
+
+object SavePdfToImage {
+
+  fun save(document: PDDocument): List<BufferedImage> {
+    val renderer = PDFRenderer(document)
+
+    return (0 until document.numberOfPages).map(renderer::renderImage)
+  }
+}
