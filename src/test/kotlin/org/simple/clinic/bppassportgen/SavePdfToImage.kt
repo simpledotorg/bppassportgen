0153da994488793ff137761cc611b14d94aed01d
Vinay S Shenoy <vinay@obvious.in>
2019-10-02 09:28:46 +0530

    Change approval images to grayscale
    The earlier images were in RGB, but different systems would render
    things ever so slightly differently, so the approvals would work on
    only one system, but not on the other. This is an experiment to see
    if it would help in fixing this problem.

--- src/test/kotlin/org/simple/clinic/bppassportgen/SavePdfToImage.kt
+++ src/test/kotlin/org/simple/clinic/bppassportgen/SavePdfToImage.kt
@@ -1,6 +1,7 @@
 package org.simple.clinic.bppassportgen
 
 import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.rendering.ImageType
 import org.apache.pdfbox.rendering.PDFRenderer
 import java.awt.image.BufferedImage
 
@@ -9,6 +10,6 @@ object SavePdfToImage {
   fun save(document: PDDocument): List<BufferedImage> {
     val renderer = PDFRenderer(document)
 
-    return (0 until document.numberOfPages).map { renderer.renderImageWithDPI(it, 150F) }
+    return (0 until document.numberOfPages).map { renderer.renderImageWithDPI(it, 150F, ImageType.GRAY) }
   }
 }
