278e76a6c8b88d6e40b89ed9f85313c15e8d5fe0
Vinay S Shenoy <vinay@obvious.in>
2019-10-05 21:04:17 +0530

    Add Renderable#Type enum

--- src/main/kotlin/org/simple/bppassportgen/renderable/Renderable.kt
+++ src/main/kotlin/org/simple/bppassportgen/renderable/Renderable.kt
@@ -5,4 +5,9 @@ import org.simple.bppassportgen.OpenedDocument
 
 interface Renderable {
   fun render(document: OpenedDocument, page: PDPage)
+
+  enum class Type {
+    PassportQrCode,
+    PassportShortcode
+  }
 }
