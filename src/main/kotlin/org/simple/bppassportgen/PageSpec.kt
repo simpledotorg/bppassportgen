8177aaea1d6915ea963db07d33fb008c4708310b
Vinay S Shenoy <vinay@obvious.in>
2019-10-05 13:57:15 +0530

    Add comment for PageSpec

--- src/main/kotlin/org/simple/bppassportgen/PageSpec.kt
+++ src/main/kotlin/org/simple/bppassportgen/PageSpec.kt
@@ -5,6 +5,10 @@ import org.simple.bppassportgen.renderable.Renderable
 /**
  * This class maintains a list of [Renderable] objects for a given
  * page in the source document
+ *
+ * Comment by author: I don't know what a good name for this is, but
+ * it signifies is this - "For every clone of the template document
+ * that is made, draw these renderables on each clone".
  **/
 data class PageSpec(private val map: Map<Int, List<Renderable>>) {
 
