c2295d9ff0947d3d1be1935bddf45f9c4e734376
Sasikanth Miriyampalli <sasikanth@obvious.in>
2022-01-29 14:44:59 +0530

    Track `templateFilePath` in `PassportsGeneratorModel`

--- src/main/kotlin/org/simple/bppassportgen/generator/PassportsGeneratorModel.kt
+++ src/main/kotlin/org/simple/bppassportgen/generator/PassportsGeneratorModel.kt
@@ -1,16 +1,22 @@
 package org.simple.bppassportgen.generator
 
 data class PassportsGeneratorModel(
-    val generatorType: GeneratorType
+    val generatorType: GeneratorType,
+    val templateFilePath: String?
 ) {
 
   companion object {
     fun create() = PassportsGeneratorModel(
-        generatorType = GeneratorType.Passport
+        generatorType = GeneratorType.Passport,
+        templateFilePath = null
     )
   }
 
   fun generatorTypeChanged(type: GeneratorType): PassportsGeneratorModel {
     return copy(generatorType = type)
+  }
+
+  fun templateFileSelected(templateFilePath: String): PassportsGeneratorModel {
+    return copy(templateFilePath = templateFilePath)
   }
 }
