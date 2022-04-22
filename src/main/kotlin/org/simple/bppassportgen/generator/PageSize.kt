ecbcbb04573fc6a5386de883c9d84a5f0227f139
Sasikanth Miriyampalli <sasikanth@obvious.in>
2022-04-22 09:34:12 +0530

    Add `PageSize` enum and default values

---
+++ src/main/kotlin/org/simple/bppassportgen/generator/PageSize.kt
@@ -0,0 +1,9 @@
+package org.simple.bppassportgen.generator
+
+enum class PageSize(val rows: Int, val columns: Int) {
+  A0(rows = 4, columns = 8),
+  A1(rows = 4, columns = 4),
+  A2(rows = 2, columns = 4),
+  A3(rows = 2, columns = 2),
+  A4(rows = 1, columns = 4)
+}
