bb67fa9f6020231d357340ef82772c1fbb910bac
Sasikanth Miriyampalli <sasikanth@obvious.in>
2022-04-22 09:44:59 +0530

    fixup! ecbcbb04573fc6a5386de883c9d84a5f0227f139

--- src/main/kotlin/org/simple/bppassportgen/generator/PageSize.kt
+++ src/main/kotlin/org/simple/bppassportgen/generator/PageSize.kt
@@ -5,5 +5,5 @@ enum class PageSize(val rows: Int, val columns: Int) {
   A1(rows = 4, columns = 4),
   A2(rows = 2, columns = 4),
   A3(rows = 2, columns = 2),
-  A4(rows = 1, columns = 4)
+  A4(rows = 1, columns = 2)
 }
